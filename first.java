c08547f5cd4c87a4aecb182cd1921f313b83e85a
Priyanshi <priyanshikasaudhan8429@gmail.com>
2024-02-26 16:33:55 +0530

    first commit

---
+++ first.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.Scanner;
+public class first {
+    public static void main(String[] args) {
+        System.out.print("Enter the any number: ");
+        Scanner sc= new Scanner(System.in);
+        double N = sc.nextInt();
+        double a= N*2;
+        System.out.println(a);
+        System.out.println("Enter your first name: ");
+        String S = sc.next();
+        System.out.println(S);
+    }
+}
